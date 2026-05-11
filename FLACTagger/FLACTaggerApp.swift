2dd08b8a0a498040e7ad765c15db944b9fa85faa
Mir.Yang <sn.gameking@gmail.com>
2026-05-11 11:47:18 +0800

    Initial Commit

---
+++ FLACTagger/FLACTaggerApp.swift
@@ -0,0 +1,17 @@
+//
+//  FLACTaggerApp.swift
+//  FLACTagger
+//
+//  Created by oldbie on 2026/5/11.
+//
+
+import SwiftUI
+
+@main
+struct FLACTaggerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
